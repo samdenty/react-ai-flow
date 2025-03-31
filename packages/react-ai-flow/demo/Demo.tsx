e9c19be0851b34f1b1c2968446cd8de39e2b0429
Sam Denty <sam@samdenty.com>
2025-03-31 04:41:35 +0100

    fix: remove vibrations from demo

--- packages/react-ai-flow/demo/Demo.tsx
+++ packages/react-ai-flow/demo/Demo.tsx
@@ -20,7 +20,7 @@ import { RadioGroup, RadioGroupItem } from "./components/ui/radio-group.js";
 import { Slider } from "./components/ui/slider.js";
 import { useFakeMessages } from "./useFakeMessages.js";
 
-enableIOSVibrationWithPopup();
+// enableIOSVibrationWithPopup();
 
 function ScrollToBottom() {
 	const { isAtBottom, scrollToBottom } = useStickToBottomContext();
